846c79debb95aed3dd76ff0ec929ce9840522634
adymilk <924114103@qq.com>
2020-11-04 13:59:17 +0800

    q

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@ header('Content-type:text/html;charset=utf-8');
 
 
 //连接认证
-$link = mysqli_connect('localhost:3306','root','52b228hq') or die('数据库连接失败！');
+$link = mysqli_connect('localhost:3306','root','root') or die('数据库连接失败！');
 
 //设定字符集
 mysqli_query($link,'set names utf8');
